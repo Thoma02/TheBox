e1a7d10b8ff2190b456d6a9bac89849dcad12ef3
Thoma02 <tomi20020509@gmail.com>
2023-03-29 15:50:28 +0200

    Made an "About us" section and styled it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Sections/Header/Header';
 import OurMission from './components/Sections/OurMission/OurMission';
+import OurReputation from './components/Sections/OurReputation/OurReputation';
+import AboutUs from './components/Sections/AboutUs/AboutUs';
 
 function App() {
   return (
     <>
       <Header />
       <OurMission />
+      <OurReputation />
+      <AboutUs />
     </>
   );
 }
