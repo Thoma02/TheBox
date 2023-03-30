900d18fe987a4a256f5be7dfc9c1c48f4d757298
Thoma02 <tomi20020509@gmail.com>
2023-03-30 18:15:11 +0200

    Added a form and styled it, I also added an asterisk to show whenever a required field is empty. There is a small proble with the selector, that I need to fix

---
+++ src/components/Sections/Form/Form.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./Form.css"
+
+export default function Form() {
+
+    const [nameValue, setNameValue] = useState("");
+    const [isNameEmpty, setIsNameEmpty] = useState(true);
+    const [emailValue, setEmailValue] = useState("");
+    const [isEmailEmpty, setIsEmailEmpty] = useState(true);
+    const [selectValue, setSelectValue] = useState("");
+    const [isSelectEmpty, setIsSelectEmpty] = useState(true);
+  
+    const handleNameChange = (e) => {
+        setNameValue(e.target.value);
+        setIsNameEmpty(false);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmailValue(e.target.value);
+        setIsEmailEmpty(false);
+      };
+
+      const handleSelectChange = (e) => {
+        setSelectValue(e.target.value);
+        setIsSelectEmpty(false);
+      };
+  
+      const handleBlur = () => {
+        if (nameValue === "") {
+            setIsNameEmpty(true);
+        }
+        if (emailValue === "") {
+          setIsEmailEmpty(true);
+        }
+        };
+
+    return (
+        <div id="form-container">
+            <h1>What can us do for you?</h1>
+            <p id="text">We are ready to work on a project of any complexity, whether it’s commercial or residential.</p>
+            <form action="">
+                <div className="input-fields">
+                    <div className="inputsAndLabels">
+                        <input type="text" id="name" name="name" required="required" placeholder="Your Name" 
+                            value={nameValue}
+                            onChange={handleNameChange}
+                            onBlur={handleBlur}
+                        />
+                        <label for="name" style={{ display: isNameEmpty ? "inline" : "none" }}></label>
+                    </div>
+                    <div className="inputsAndLabels">
+                        <input type="email" id="email" name="email" required="required" placeholder="Email"  
+                            value={emailValue}
+                            onChange={handleEmailChange}
+                            onBlur={handleBlur}
+                        />
+                        <label id="label-email" for="email" style={{ display: isEmailEmpty ? "inline" : "none" }}></label>
+                    </div>
+                </div>
+                <div className="input-fields">
+                    <label id="label-select" style={{ display: isSelectEmpty ? "inline" : "none" }}></label>
+                    <select name="reasons" id="reasons"
+                         value={selectValue}
+                         onChange={handleSelectChange}
+                    >
+                        <option selected disabled>Reason for Contacting</option>
+                        <option>Option1</option>
+                        <option>Option2</option>
+                        <option>Option3</option>
+                        <option>Option4</option>
+                        <option>Option5</option>
+                    </select>
+                    <input type="text" placeholder="Phone"/>
+                    <img src="./Assets/Sections/Form/vector.svg" alt=""/>
+                </div>
+                <textarea name="" id="" placeholder="Message"></textarea>
+                <p id="indicator"><span className="red">* </span>indicates a required field</p>
+                <input id="button" type="submit"/>
+            </form>
+        </div>
+    )
+}
